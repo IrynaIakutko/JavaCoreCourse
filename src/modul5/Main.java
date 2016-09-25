f6604724294cc04001dcc19deb5b0658c5d4d85b
IrynaIakutko <yakirvikt@gmail.com>
2016-09-25 19:25:01 +0300

    Homework modul  all5

--- src/modul5/Main.java
+++ src/modul5/Main.java
@@ -8,24 +8,36 @@ public class Main {
 
         Controller controller = new Controller();
         API[] api1=controller.getApis();
+        //Room[] room;
+        Room[] room1;
+        Room[] room2;
+        Room[] room3;
+     //   Room[] room4;
+        Room[] room5;
 
 
         DAOimpl daOimpl=new DAOimpl();
-//API bookingApi =
+
         Room[] room= controller.requestRooms(2900, 4, "New Hotel","Krakov" );
 
         for(int i=0;i<room.length;i++)
         {daOimpl.save(room[i]);}
 
-       room=controller.requestRooms(300, 3 , "Warsaw", "Sofitel Warsaw Victoria");
-        for(Room k : room)
+       room1=controller.requestRooms(300, 3 , "Warsaw", "Sofitel Warsaw Victoria");
+        for(Room k : room1)
         {daOimpl.save(k);}
-        room=controller.requestRooms(555, 4, "Paris","Hotel Panache");
-        for(Room k : room)
+        room2=controller.requestRooms(555, 4, "Paris","Hotel Panache");
+        for(Room k : room2)
         {daOimpl.save(k);}
-       room= controller.check(api1[1],api1[2]);
-        for(int i=0;i<room.length;i++)
-        {daOimpl.save(room[i]);}
+       room3= controller.check(api1[1],api1[2]);
+        for(int i=0;i<room3.length;i++)
+        {daOimpl.save(room3[i]);}
+     Room[] room4= controller.check(api1[1],api1[0]);
+        for(int i=0;i<room4.length;i++)
+        {daOimpl.save(room4[i]);}
+        room5= controller.check(api1[2],api1[0]);
+        for(int i=0;i<room5.length;i++)
+        {daOimpl.save(room5[i]);}
 
 
 
