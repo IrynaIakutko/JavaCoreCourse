afcac1e8b03310422c289ab5a27252d0bcfbf349
IrynaIakutko <yakirvikt@gmail.com>
2016-10-16 22:14:19 +0300

    Homework modul  8 .4

--- src/modul8/UserDAO.java
+++ src/modul8/UserDAO.java
@@ -7,7 +7,18 @@ public class UserDAO extends  AbDAOImpl<User> {
     public UserDAO(List<User> list){
         super();
         super.getList();
-
     }
-
+    public User save(User user){
+        save(user);
+        return user;
+    };
+    public void delete(User user){
+        delete(user);
+    };
+    public void deleteById(long id){
+        deleteById(id);
+    };
+    public User get(long id){
+       return get(id);
+    };
 }
